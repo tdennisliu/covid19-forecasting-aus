8d67dc70b95b89c1b6101070293f4a229c7b941e
Dennis Liu <tdennisliu@gmail.com>
2020-07-02 13:01:39 +0930

    fixed appropriate assignment of alphas into abc form

--- run_sim.py
+++ run_sim.py
@@ -74,25 +74,23 @@ for state in states:
         #qi_prior = beta(17, 3, size=10000)
         qi_prior = [qi_d[state]]
         qs_prior = [local_detection[state]]
-        gam =np.minimum(2,gamma(4,0.25, size=1000))
-        ps_prior = beta(5,5,size=1000)
-        alpha_s_prior = 1/(ps_prior + (1- ps_prior)*gam)
+        gam =np.maximum(0.1,np.minimum(2,gamma(4,0.25, size=1000)))
+        ps_prior = beta(10,10,size=1000)
 
     else:
         qi_prior = [qi_d[state]]
         qs_prior = [local_detection[state]]
-        gam =1/3
+        gam =[1/3]
         ps_prior = 0.7
-        alpha_s_prior = [1/(ps_prior + (1- ps_prior)*gam)]
         ps_prior= [ps_prior]
-        alpha_a_prior = [gam*alpha_s_prior[0]]
+
     for i,cat in enumerate(initial_people):
         people[i] = Person(0,0,0,0,cat)
     
     if state in ['VIC']:
         forecast_dict[state] = Forecast(current[state],
         state,start_date,people,
-        alpha_i= 1, k =0.1,alpha_s_list=alpha_s_prior,
+        alpha_i= 1, k =0.1,gam_list=gam,
         qs_list=qs_prior,qi_list=qi_prior,
         qua_ai=1,qua_qi_factor=1,qua_qs_factor=1,
         forecast_R =forecast_type, R_I = R_I,forecast_date='2020-06-29',
@@ -102,7 +100,7 @@ for state in states:
     elif state in ['NSW']:
         forecast_dict[state] = Forecast(current[state],
         state,start_date,people,
-        alpha_i= 1, k =0.1,alpha_s_list=alpha_s_prior,
+        alpha_i= 1, k =0.1,gam_list=gam,
         qs_list=qs_prior,qi_list=qi_prior,
         qua_ai=1,qua_qi_factor=1,qua_qs_factor=1,
         forecast_R =forecast_type, R_I = R_I,forecast_date='2020-06-29',
@@ -112,7 +110,7 @@ for state in states:
     elif state in ['ACT','NT']:
         forecast_dict[state] = Forecast(current[state],
         state,start_date,people,
-        alpha_i= 0.5, k =0.1,alpha_s_list=alpha_s_prior,
+        alpha_i= 0.5, k =0.1,gam_list=gam,
         qs_list=qs_prior,qi_list=qi_prior,
         qua_ai=1,qua_qi_factor=1,qua_qs_factor=1,
         forecast_R =forecast_type, R_I = R_I,forecast_date='2020-06-29',
@@ -122,7 +120,7 @@ for state in states:
     else:
         forecast_dict[state] = Forecast(current[state],state,
         start_date,people,
-        alpha_i= 0.5, k =0.1,alpha_s_list=alpha_s_prior,
+        alpha_i= 0.5, k =0.1,gam_list=gam,
         qs_list=qs_prior,qi_list=qi_prior,
         qua_ai=1,qua_qi_factor=1,qua_qs_factor=1, 
         forecast_R = forecast_type , R_I = R_I,forecast_date='2020-06-29',

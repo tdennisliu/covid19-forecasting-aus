2fc63bab88da7d45737f250717a3be8c6de6c260
Dennis Liu <tdennisliu@gmail.com>
2020-07-17 17:07:36 +0930

    DP's code to run EpiEstim

---
+++ model/LSHTM_r/install_lshtm_packages_R.r
@@ -0,0 +1,8 @@
+# This analysis uses on a number of packages that are under active development
+# and changing rapidly as the pandemic progresses. The specific versions of the
+# LSHTM modelling packages we used can be installed from GitHub using the
+# folowing code. The remotes package used to install them is available on CRAN.
+
+remotes::install_github("epiforecasts/scoringutils@6efcbf8")
+remotes::install_github("epiforecasts/EpiSoon@67ae487")
+remotes::install_github("epiforecasts/EpiNow@3236137")

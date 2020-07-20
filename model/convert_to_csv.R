14945f14d9fc3d1a1aba0fa23f39724ad965f44f
Dennis Liu <tdennisliu@gmail.com>
2020-07-20 09:53:27 +0930

    collates Reff from multiple runs of EpiEstim

--- model/convert_to_csv.R
+++ model/convert_to_csv.R
@@ -1,18 +1,21 @@
 # Script to read in R_eff RDS files and output into csv
 setwd('~/Documents/GitHub/covid19-forecasting-aus/data/LSHTM_Reff_estimates/')
-date <- '2020-07-16'
+
+dates <- c('2020-04-26','2020-06-28','2020-07-16')
 states <- c('WA','VIC','TAS','SA','QLD','NSW')
 df <- data.frame()
 for (i in 1:length(states)) {
-  filename <- paste(states[i],'/',date,'/','bigr_estimates','.RDS',sep='')
-  df_temp <- readRDS(filename)
-  df_temp$state <- rep(states[i],dim(df_temp)[1])
-
-  df<- rbind(df,df_temp)
+  for (j in 1:length(dates)) {
+    filename <- paste(states[i],'/',dates[j],'/','bigr_estimates','.RDS',sep='')
+    df_temp <- readRDS(filename)
+    df_temp$state <- rep(states[i],dim(df_temp)[1])
+    df_temp$date_of_analysis <- rep(dates[j], nrow(df_temp))
   
+    df<- rbind(df,df_temp)
+  }
 }
 #Remove column that is just a list of other columns and rt_type is same as type
 df <- subset(df,select=!(names(df) %in% c('R0_range','rt_type')))
 
-filename <- paste('R_eff_',date,'.csv',sep='')
+filename <- 'Reff_LSHTM.csv'
 write.csv(df,file=filename,row.names = F)

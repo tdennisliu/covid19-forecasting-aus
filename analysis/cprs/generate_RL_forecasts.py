84419d2af01554007010cddb404943c74c3f99cb
Dennis Liu <tdennisliu@gmail.com>
2020-08-27 11:21:38 +0930

    fixes missing zero on leading dates error

--- analysis/cprs/generate_RL_forecasts.py
+++ analysis/cprs/generate_RL_forecasts.py
@@ -75,7 +75,11 @@ cprs_dates = pd.date_range(cprs_start_date, cprs_end_date, freq='7D')
 for data_date in cprs_dates:
     print(data_date)
     df_samples = read_in_posterior(date = data_date.strftime("%Y-%m-%d"))
-    cases = read_in_cases(data_date.strftime('%d%b%Y'))
+    if data_date < '2020-06-02':
+        #no leading zero on early dates
+        cases = read_in_cases(data_date.strftime('%d%b%Y')[1:])
+    else:
+        cases = read_in_cases(data_date.strftime('%d%b%Y'))
     
     one_month = data_date + timedelta(days= 28)
 

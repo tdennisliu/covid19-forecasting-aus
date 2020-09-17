8d32013043d4d3abce253c5990332f7cabe962aa
tdennisliu <tdennisliu@gmail.com>
2020-09-17 13:39:39 +0930

    shorten phoenix job time and mem requirements

--- model/contact_tracing/phoenix_ct_days.sh
+++ model/contact_tracing/phoenix_ct_days.sh
@@ -3,8 +3,8 @@
 #SBATCH --qos=express
 #SBATCH -N 1
 #SBATCH -n 10
-#SBATCH --time=1-00:00:00
-#SBATCH --mem=70GB
+#SBATCH --time=03:00:00
+#SBATCH --mem=30GB
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL

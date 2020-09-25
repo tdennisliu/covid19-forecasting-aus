9fddaa1460bf0e32d7c3c666d177b78277c1c062
Dennis Liu <tdennisliu@gmail.com>
2020-09-25 14:52:56 +0930

    clarify how to use in script comments

--- analysis/cprs/run_posteriors.sh
+++ analysis/cprs/run_posteriors.sh
@@ -14,11 +14,8 @@ module load Python/3.6.1-foss-2016b
 source $FASTDIR/virtualenvs/bin/activate
 
 
-#dates=("2020-04-01" "2020-04-08" "2020-04-15" "2020-04-22" "2020-04-29" "2020-05-06" 
-#"2020-05-13" "2020-05-20" "2020-05-27" "2020-06-03" "2020-06-10" "2020-06-17" "2020-06-24" 
-#"2020-07-01" "2020-07-08" "2020-07-15" "2020-07-22")
 
-python analysis/cprs/generate_posterior.py $1
-python analysis/cprs/generate_RL_forecasts.py $1
+python analysis/cprs/generate_posterior.py $1 #comment out argument for cprs
+python analysis/cprs/generate_RL_forecasts.py $1 #as above
 
 deactivate

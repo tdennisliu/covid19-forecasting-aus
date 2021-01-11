a0b151f128c2ea3470f4cf008fbc03127159efe8
Dennis Liu <tdennisliu@gmail.com>
2021-01-11 16:38:37 +1030

    initial commit for num of days script

---
+++ num_days.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sys import argv
+start_s ="2020-09-01"
+start_dt = pd.to_datetime(start_s,format="%Y-%m-%d")
+
+if len(argv)>1:
+    num_forecast_days = int(argv[1])
+else:
+    num_forecast_days = 35
+
+end_date = pd.to_datetime("today",format="%Y-%m-%d") + pd.Timedelta(days =num_forecast_days)
+
+num_days = (end_date - start_dt).days
+print("Number of days to simulate: %i days" % num_days)

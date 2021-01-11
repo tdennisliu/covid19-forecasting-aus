8786ee15ffdbb8a6df6e4f07745d0519bab360c6
Dennis Liu <tdennisliu@gmail.com>
2021-01-11 16:30:42 +1030

    increased total sims in final run

--- forecast_pipeline.sh
+++ forecast_pipeline.sh
@@ -14,8 +14,8 @@ echo $jid3
 
 jid4=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_collate_states.sh 200 ${NDAYS} ${DATE})
 
-jid5=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_all_states.sh 5000 ${NDAYS} ${DATE})
+jid5=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_all_states.sh 12000 ${NDAYS} ${DATE})
 
-jid6=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid5 phoenix_final_plots_csv.sh 5000 ${NDAYS} ${DATE})
+jid6=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid5 phoenix_final_plots_csv.sh 12000 ${NDAYS} ${DATE})
 
 

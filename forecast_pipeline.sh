1918a873a4bb3105557a76b5c6119fb347836725
Tobin South <tobin.south@gmail.com>
2021-03-01 12:11:07 +1030

    Increase simulation count

--- forecast_pipeline.sh
+++ forecast_pipeline.sh
@@ -14,12 +14,12 @@ echo $jid3
 
 jid4=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_collate_states.sh 200 ${NDAYS} ${DATE})
 
-jid5=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_all_states.sh 12000 ${NDAYS} ${DATE})
+jid5=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_all_states.sh 20000 ${NDAYS} ${DATE})
 
-jid6=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid5 phoenix_final_plots_csv.sh 12000 ${NDAYS} ${DATE})
+jid6=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid5 phoenix_final_plots_csv.sh 20000 ${NDAYS} ${DATE})
 
 
-jid7=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid6 phoenix_all_states.sh 12000 ${NDAYS} ${DATE} None UK)
+jid7=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid6 phoenix_all_states.sh 20000 ${NDAYS} ${DATE} None UK)
 
-jid8=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid7 phoenix_collate_states.sh 12000 ${NDAYS} ${DATE} UK)
+jid8=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid7 phoenix_collate_states.sh 20000 ${NDAYS} ${DATE} UK)
 

1ec170f9e94b5c67ff40f7b8e007ad6b3a956411
Tobin South <tobin.south@gmail.com>
2021-03-08 15:12:55 +1030

    Update pipeline runcode

--- forecast_pipeline.sh
+++ forecast_pipeline.sh
@@ -4,22 +4,17 @@ DATE=$1
 NDAYS=$2
 
 jid1=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au phoenix_run_estimator.sh ${DATE})
-echo $jid1
 
 jid2=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1 analysis/cprs/run_posteriors.sh ${DATE})
-echo $jid2
-
-jid3=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2 phoenix_all_states.sh 200 ${NDAYS} ${DATE}) 
-echo $jid3
-
-jid4=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_collate_states.sh 200 ${NDAYS} ${DATE})
-
-jid5=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2,$jid3 phoenix_all_states.sh 20000 ${NDAYS} ${DATE})
-
-jid6=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid5 phoenix_final_plots_csv.sh 20000 ${NDAYS} ${DATE})
 
 
-jid7=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid6 phoenix_all_states.sh 20000 ${NDAYS} ${DATE} None UK)
+jid4=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid1,$jid2 phoenix_all_states.sh 30000 ${NDAYS} ${DATE})
 
-jid8=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid7 phoenix_collate_states.sh 20000 ${NDAYS} ${DATE} UK)
+jid5=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid4 phoenix_final_plots_csv.sh 30000 ${NDAYS} ${DATE})
+echo "Normal Run:", $jid4, $jid5
 
+
+jid6=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid5 phoenix_all_states.sh 30000 ${NDAYS} ${DATE} None UK)
+
+jid7=$(sbatch --parsable --mail-user=$USER@adelaide.edu.au --dependency=afterok:$jid6 phoenix_final_plots_csv.sh 30000 ${NDAYS} ${DATE} UK)
+echo "VoC Run:", $jid6, $jid7

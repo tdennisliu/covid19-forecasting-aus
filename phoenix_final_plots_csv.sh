438d8c02dc8beabe201bbf206721f1913c735a0b
Tobin South <tobin.south@gmail.com>
2021-03-08 15:12:38 +1030

    Pass VoC param to record_to_csv.py

--- phoenix_final_plots_csv.sh
+++ phoenix_final_plots_csv.sh
@@ -5,7 +5,6 @@
 #SBATCH -n 1
 #SBATCH --time=00:30:00
 #SBATCH --mem=20GB
-#SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=$USER@adelaide.edu.au
@@ -15,6 +14,6 @@ module load Python/3.6.1-foss-2016b
 source ../virtualenvs/bin/activate
 
 python collate_states.py $1 $2 $3 $4
-python analysis/record_to_csv.py $1 $2 R_L $3
+python analysis/record_to_csv.py $1 $2 R_L $3 $4
 
 deactivate
